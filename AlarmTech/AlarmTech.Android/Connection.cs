f022cbda016fe3ea09f6c595d761be7c514830b0
Yul isai Villegas <yulvillegasglz@gmail.com>
2022-03-01 19:52:31 -0600

    version 1.0

---
+++ AlarmTech/AlarmTech.Android/Connection.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlarmTech.Droid
+{
+    public class Connection
+    {
+        public MySqlConnection con;
+        public bool TryConnection(Context context)
+        {
+
+        }
+    }
+}
+// bd: alarm_tech_db
+// usuario: kn_mct
+// contraseña: 7(DR@IRa7UMAMSlH
+// correo: yulvillegasglz@gmail.com
+// host: db4free.net
